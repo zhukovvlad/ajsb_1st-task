0a7eb4678adedf639ba2bee214bc1d7942a41c96
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-01-29 23:40:42 +0300

    Commit with webpack-merge

---
+++ webpack.dev.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+});
---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({}),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+});
