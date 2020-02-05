dfa8ae513090a821f7341f46d81cb028c53dbe33
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-02-05 23:18:50 +0300

    Modified additional webpack configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
